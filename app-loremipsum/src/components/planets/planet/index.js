c1722c557b1e13d361338f67a944bce8032bf4b8
DiogoBarrosCode <diogobarros.code@gmail.com>
2023-01-08 12:49:17 -0300

    new controlled form satellites

--- app-loremipsum/src/components/planets/planet/index.js
+++ app-loremipsum/src/components/planets/planet/index.js
@@ -1,45 +1,57 @@
-import React, {Fragment, useState, useEffect} from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import DescriptionWhitLink from "../../shared/DescriptionWithLink";
 import GrayImg from "../../shared/gray_img";
+import Form from "./Form";
 
-async function getSatellites(id){
-    let response = await fetch(`http://localhost:3000/api/${id}.json`)
-    let data = await response.json();
-    return data;
+async function getSatellites(id) {
+  let response = await fetch(`http://localhost:3000/api/${id}.json`);
+  let data = await response.json();
+  return data;
 }
 
 const Planet = (props) => {
-    const [satellites, setSatellites] = useState([]);
+  const [satellites, setSatellites] = useState([]);
 
-    useEffect(() => {
-        getSatellites(props.id).then(data => {
-            setSatellites(data['satellites'])
-        })
-    }, [])
+  useEffect(() => {
+    getSatellites(props.id).then((data) => {
+      setSatellites(data["satellites"]);
+    });
+  }, []);
 
-        let title;
-        if(props.title_with_underline){
-            title = <h4><u>{props.name}</u></h4>
-        } else {
-            title = <h4>{props.name}</h4>
-        }
+  const addSatellite = (new_satellite) => {
+    setSatellites([...satellites, new_satellite])
+  }
 
-        return (
-            //<div onClick={() => props.clickOnPlanet(props.name)}>
-            <div>
-                {title}
-                <DescriptionWhitLink description={props.description} link={props.link}/>
-                <GrayImg img_url={props.img_url} gray={props.gray}/>
-                <h4>Satélites</h4>
-                <ul>
-                    {satellites.map((satellite, index) =>
-                        <li key={index}>{satellite.name}</li>
-                    )}
-                </ul>
-                
-                <hr/>
-            </div>
-        )
-    }
+  let title;
+  if (props.title_with_underline) {
+    title = (
+      <h4>
+        <u>{props.name}</u>
+      </h4>
+    );
+  } else {
+    title = <h4>{props.name}</h4>;
+  }
+
+  return (
+    //<div onClick={() => props.clickOnPlanet(props.name)}>
+    <div>
+      {title}
+      <DescriptionWhitLink description={props.description} link={props.link} />
+      <GrayImg img_url={props.img_url} gray={props.gray} />
+      <h4>Satélites</h4>
+      <hr/>
+        <Form addSatellite={addSatellite}/>
+      <hr/>
+      <ul>
+        {satellites.map((satellite, index) => (
+          <li key={index}>{satellite.name}</li>
+        ))}
+      </ul>
+
+      <hr />
+    </div>
+  );
+};
 
 export default Planet;
